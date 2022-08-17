bb766a62a2c31bc60479878e2e31f47d7cd30b7a
gregoryput <gregory-sanchez7878@hotmail.com>
2022-08-16 20:47:23 -0400

    buburja

---
+++ MetodoDeBurbuja/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿
+namespace MetodoDeBurbuja
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(9, 34);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(131, 256);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(208, 31);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 24);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Ordenar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(304, 299);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "Algoritmo de burbuja";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ MetodoDeBurbuja/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MetodoDeBurbuja
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        public void ordenar(int[] lista)
+        {
+            int aux = 0;
+            //textBox1.Text = "Vector desordenado" + Environment.NewLine;
+            for (int i = 0; i < lista.Length-1 ; i++)
+            {
+                for (int x = 0; x < lista.Length -1; x++)
+                {
+                    if (lista[x] > lista[x + 1])
+                    {
+                        aux = lista[x];
+                        lista[x] = lista[x + 1];
+                        lista[x + 1] = aux;
+                    }
+                }
+
+            }
+            textBox1.Text += "Vector Ordenado" + Environment.NewLine;
+
+            foreach (int item in lista)
+            {
+                textBox1.Text += item + Environment.NewLine;
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[] lista = { 10, 17, 1, 4, 6, 2, 3 };
+            textBox1.Text = "Vector Desordenado"+ Environment.NewLine;
+
+            foreach (int numero in lista)
+            {
+                textBox1.Text += numero + Environment.NewLine;
+            }
+            ordenar(lista);
+
+            
+        }
+    }
+
+}
